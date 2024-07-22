75ad54db6c4b4a0841b3b224b1222cc927524f3a
Ehthesham-Ahmed <117994804+Ehthesham-Ahmed@users.noreply.github.com>
2024-07-22 22:52:25 +0530

    rental home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PostCard from '../components/PostCard';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch('/api/post/getposts');
+            const data = await res.json();
+            setPosts(data.posts);
+        }
+        fetchPosts();
+    }, []);
     return (
-        <div>Home</div>
+        <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+            {
+                posts && posts.length > 0 && (
+                    <div className='flex flex-col gap-6'>
+                        <h2 className='text-3xl font-bold text-center'>Rentals</h2>
+                        <div className='flex flex-wrap gap-4'>
+                            {posts.map((post) => (
+                                <PostCard key={post._id}
+                                    post={post} />
+                            ))}
+                        </div>
+                        <Link to={'/search'}
+                            className='text-lg text-teal-500 hover:underline text-center'>
+                            Filter
+                        </Link>
+                    </div>
+                )
+            }
+        </div>
     )
 }
